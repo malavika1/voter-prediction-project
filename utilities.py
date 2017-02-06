1acaa24e091b219d7becf916230bb1fc3073b629
mortonrj <morton.rjt@gmail.com>
2017-02-05 18:15:18 -0800

    Changed casing

--- utilities.py
+++ utilities.py
@@ -7,7 +7,7 @@ from io import BytesIO
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 
-def importTrainData(train_file='train_2008.csv', ):
+def import_train_data(train_file='train_2008.csv', ):
     data = np.genfromtxt(train_file, delimiter=',', dtype=str)[1:]
     data_x = data[:, list(range(0, 382))]
     data_y = data[:, [382]]
@@ -15,11 +15,13 @@ def importTrainData(train_file='train_2008.csv', ):
 
     return data_x, data_y
 
-
-def importTestData(test_file='test_2008.csv'):
+def import_test_data(test_file='test_2008.csv'):
     data = np.genfromtxt(test_file, delimiter=',', dtype=str)[1:]
     data_x = data[:, list(range(0, 382))]
 
     return data_x
+
+#def normalize_data(train_data_x, train_data_y, test_data_x):
+
 
 # normalize minus mean divided by standard deviation
