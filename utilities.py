5ddfd60f086ec6c024fc54973339a45c820c2002
mortonrj <morton.rjt@gmail.com>
2017-02-05 18:23:35 -0800

    Took out id from data

--- utilities.py
+++ utilities.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def import_train_data(train_file='train_2008.csv', ):
     data = np.genfromtxt(train_file, delimiter=',', dtype=str)[1:]
-    data_x = data[:, list(range(0, 382))]
+    data_x = data[:, list(range(1, 382))]
     data_y = data[:, [382]]
     data_y = [i[0] for i in data_y]
 
@@ -13,11 +13,11 @@ def import_train_data(train_file='train_2008.csv', ):
 
 def import_test_data(test_file='test_2008.csv'):
     data = np.genfromtxt(test_file, delimiter=',', dtype=str)[1:]
-    data_x = data[:, list(range(0, 382))]
+    data_x = data[:, list(range(1, 382))]
 
     return data_x
 
-"""
+
 def normalize_data(train_data_x, train_data_y, test_data_x):
     num_cols = len(train_data_x[0])
 
@@ -32,6 +32,6 @@ def normalize_data(train_data_x, train_data_y, test_data_x):
     xt1 = (xt - mean_x1) / std_x1
     x2 = (x2 - mean_x2) / std_x2
     xt2 = (xt - mean_x2) / std_x2
-"""
+
 # normalize minus mean divided by standard deviation
 #
