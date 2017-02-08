ad3acb3e5e450af182255a35497d785afaa91bd0
mortonrj <morton.rjt@gmail.com>
2017-02-07 22:57:03 -0800

    Added neural network mlp to voting

--- voting.py
+++ voting.py
@@ -7,16 +7,18 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 
 # Get the normalized data
 X_train, y_train, X_test = ut.import_data()
 
-#clf_linsvc = LinearSVC(C=0.05)
+clf_linsvc = LinearSVC(C=0.05)
 clf_adatree = AdaBoostClassifier(DecisionTreeClassifier(max_depth=2))
 clf_forest = RandomForestClassifier(10, max_depth=15)
-clf_logreg = LogisticRegression(C=0.03)
-#('LinearSVC', clf_linsvc), 
-eclf = VotingClassifier(estimators=[('Ada Dec Tree', clf_adatree), ('Random Forest', clf_forest), ('Log Reg', clf_logreg)], voting='hard')
+clf_logreg = LogisticRegression(C=0.15)
+clf_mlp = MLPClassifier(hidden_layer_sizes=(6), solver='sgd',learning_rate_init=0.01,max_iter=500)
+
+eclf = VotingClassifier(estimators=[('LinearSVC', clf_linsvc), ('Ada Dec Tree', clf_adatree), ('Random Forest', clf_forest), ('Log Reg', clf_logreg), ('Neural Network MLP', clf_mlp)], voting='hard')
 
 eclf.fit(X_train, y_train)
 scores = cross_val_score(eclf, X_train, y_train, cv=5, scoring='accuracy')
