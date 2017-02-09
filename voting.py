4262a09f0886c00546f7ebe7318f88f60ff925e1
mortonrj <morton.rjt@gmail.com>
2017-02-08 19:27:55 -0800

    Added gradient stuff to voting casssssh me ousside howbowdah

--- voting.py
+++ voting.py
@@ -8,17 +8,24 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
 
 # Get the normalized data
 X_train, y_train, X_test = ut.import_data()
 
 clf_linsvc = LinearSVC(C=0.05)
 clf_adatree = AdaBoostClassifier(DecisionTreeClassifier(max_depth=2))
-clf_forest = RandomForestClassifier(88, max_depth=5)
-clf_logreg = LogisticRegression(C=0.03)
-clf_mlp = MLPClassifier(hidden_layer_sizes=(5), solver='adam',learning_rate_init=0.01,max_iter=500)
+clf_forest = RandomForestClassifier(10, max_depth=15)
+clf_logreg = LogisticRegression(C=0.15)
+#clf_mlp = MLPClassifier(hidden_layer_sizes=(5), solver='adam',learning_rate_init=0.007,max_iter=500)
+parameters = {
+ 'hidden_layer_sizes': [(3), (4), (5), (6), (5, 2), (4,2), (3,2), (6, 3), (5, 3), (4, 3)],
+ 'solver': ['adam', 'sgd']
+ }
+model = MLPClassifier(verbose=True, learning_rate_init=0.07,max_iter=500, solver='adam')
+clf_mlp_grid = GridSearchCV(estimator=model,  param_grid=parameters)
 
-eclf = VotingClassifier(estimators=[('LinearSVC', clf_linsvc), ('Ada Dec Tree', clf_adatree), ('Random Forest', clf_forest), ('Log Reg', clf_logreg), ('Neural Network MLP', clf_mlp)], voting='hard')
+eclf = VotingClassifier(estimators=[('LinearSVC', clf_linsvc), ('Ada Dec Tree', clf_adatree), ('Random Forest', clf_forest), ('Log Reg', clf_logreg), ('Neural Network MLP Grid', clf_mlp_grid)], voting='hard')
 
 eclf.fit(X_train, y_train)
 scores = cross_val_score(eclf, X_train, y_train, cv=5, scoring='accuracy')
