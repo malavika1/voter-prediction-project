9d40c510df951ca51f98f020ee65d47b27d4b8cd
Malavika Venkatesan <malavika1996@hotmail.com>
2017-02-08 18:16:30 -0800

    fixed the voting parameters

--- voting.py
+++ voting.py
@@ -14,8 +14,8 @@ X_train, y_train, X_test = ut.import_data()
 
 clf_linsvc = LinearSVC(C=0.05)
 clf_adatree = AdaBoostClassifier(DecisionTreeClassifier(max_depth=2))
-clf_forest = RandomForestClassifier(10, max_depth=15)
-clf_logreg = LogisticRegression(C=0.15)
+clf_forest = RandomForestClassifier(88, max_depth=5)
+clf_logreg = LogisticRegression(C=0.03)
 clf_mlp = MLPClassifier(hidden_layer_sizes=(5), solver='adam',learning_rate_init=0.01,max_iter=500)
 
 eclf = VotingClassifier(estimators=[('LinearSVC', clf_linsvc), ('Ada Dec Tree', clf_adatree), ('Random Forest', clf_forest), ('Log Reg', clf_logreg), ('Neural Network MLP', clf_mlp)], voting='hard')
